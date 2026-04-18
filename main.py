001671a12fbd696aaa9e49602457df0dec166e6b
𝐂𝐨𝐥𝐢𝐧 <colinuzb@gmail.com>
2026-04-18 14:57:59 +0000

    ColinBot

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from aiogram.fsm.storage.memory import MemoryStorage
 API_TOKEN = ''
 
 # O'ZGARISH #1: Adminlar ro'yxati (List) qilib yoziladi
-ADMINS = [7788334322, 8075485288, 12345678]
+ADMINS = [7788334322, 8075485288, 5748209449]
 
-CHANNELS = ["@colinuzb", "@colincode", "@ibrohimweb"]
+CHANNELS = ["@colinuzb", "@colincode", "@ibrohimweb", "@JovaMc"]
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
